04e55809dcf4a24891b49f8490f46a7bce40a7ed
Veeti-1 <veeti.salomaa155@gmail.com>
2026-02-03 16:53:32 +0200

    painauslogiikka

--- components/TaskList.tsx
+++ components/TaskList.tsx
@@ -1,6 +1,6 @@
 import AsyncStorage from '@react-native-async-storage/async-storage';
 import React, { useEffect, useState } from 'react';
-import { StyleSheet, Text, TextInput, View,Button } from 'react-native';
+import { StyleSheet, Text, TextInput, View,Button, Pressable } from 'react-native';
 import { SwipeListView } from 'react-native-swipe-list-view';
 
 const KEY = 'TASK_LIST_ITEMS';
@@ -10,12 +10,15 @@ interface Task{
   id:string;
   name:string;
   description:string;
+  isMarkedDone:Boolean;
+ 
 }
 
 export default function TaskList(){
     const [tasks,setTasks] = useState<Task[]>([])
     const [taskTitle,setTaskTitle] = useState('')
     const [description,setdDscription] = useState('')
+    
     
     const [id, setId] = useState<Number>(1)
      useEffect(()=>{
@@ -41,12 +44,12 @@ export default function TaskList(){
         if(taskTitle.trim() && description.trim()){
             setTasks(prev =>[
             ...prev,
-            {id:id.toString(), name:taskTitle, description:description}
+            {id:id.toString(), name:taskTitle, description:description, isMarkedDone:false}
         ]) 
         setTaskTitle('')
         setdDscription('')
-        
         }
+      
     }
     return(
         <View style={styles.container}>
@@ -72,27 +75,32 @@ export default function TaskList(){
                 }}
                 />
             </View>
-                
+            
                
                 <SwipeListView
                 data={tasks}
                 keyExtractor={item=>item.id}
                 renderItem={({item})=>(
-            <View style={styles.list}>
-                    <Text>
+                
+                <View style={styles.list}>
+                    <Text onPress={()=>{
+                        styles.textCliked
+                    }}>
                         {item.name} id: {item.id}
-                    </Text>
-                    <Text>
+                        <Text>
                         {item.description}
                     </Text>
-                   </View>
+                    </Text>
+                    
+                </View>
+               
                 )}
                 renderHiddenItem={()=><View style={styles.rowBack}/>}
                 rightOpenValue={-75}
                 disableRightSwipe
-
+                
                 />
-
+             
                 
         </View>
     );
@@ -119,6 +127,15 @@ justifyContent: 'center',
 paddingRight: 20,
   },
   input:{
+    margin:5,
 
+    borderColor:'#000000',
+    borderWidth:5,
+  },
+  text:{
+
+  },
+  textCliked:{
+    textDecorationLine:'line-through'
   }
 });
